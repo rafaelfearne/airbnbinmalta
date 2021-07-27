4d412e558a13f360f25b11901441616256205c28
rafaelfearne <63465628+rafaelfearne@users.noreply.github.com>
2021-07-27 10:13:18 +0200

    Update abnb.py

--- abnb.py
+++ abnb.py
@@ -72,5 +72,5 @@ result= f"The price per night is approximately **€{resultraw:.2f}**."
 
 if 'test' not in st.session_state:
     st.session_state.test = resultraw
-    st.markdown('st.session_state.test,')
+    st.write('Previous Result = ', st.session_state.test)
 
